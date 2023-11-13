0565f490b3b310ded0437681ad3ecdc8d3467ba7
Danika Peet <DPeet@lcs.on.ca>
2023-11-12 19:26:09 -0500

    I have drawn the background

--- Apple music/ContentView.swift
+++ Apple music/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+        
+        //draw background of apple music
+        Rectangle()
         .padding()
+    
     }
 }
 
