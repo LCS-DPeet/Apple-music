cdb8619668f127c61b664d570754435a3a1bd3cc
Danika Peet <DPeet@lcs.on.ca>
2023-11-12 19:23:45 -0500

    Initial Commit

---
+++ Apple music/Apple_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_musicApp.swift
+//  Apple music
+//
+//  Created by Danika Peet on 2023-11-12.
+//
+
+import SwiftUI
+
+@main
+struct Apple_musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
